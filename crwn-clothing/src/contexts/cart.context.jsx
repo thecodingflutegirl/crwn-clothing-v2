aa09ddefccd586371778b822c20f68824bf8c4ae
Julia Evers <thatflutegirl12@gmail.com>
2023-01-10 22:58:25 -0500

    added in our addToCart feature

--- crwn-clothing/src/contexts/cart.context.jsx
+++ crwn-clothing/src/contexts/cart.context.jsx
@@ -1,12 +1,43 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+
+const addCartItem = (cartItems, productToAdd) => {
+  // find if cartItems contains productToAdd
+  const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id);
+  //if found increment quantity 
+  if(existingCartItem) {
+    return cartItems.map((cartItem) => cartItem.id === productToAdd.id 
+      ? { ...cartItem, quantity: cartItem.quantity + 1 } 
+      : cartItem 
+    );
+  }
+  //return newarray with modified cartItems / new cart items
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+}
 
 export const CartContext = createContext({
   isCartOpen: false,
-  setIsOpen: () => {},
+  setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  cartCount: 0
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  console.log("is cart open set?");
+  const [cartItems, setCartItems ] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
+    setCartCount(newCartCount);
+  }, [cartItems])
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+    console.log("add item to cart has been called")
+  }
+
+  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
--- crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -15,7 +15,7 @@ import "./navigation.styles.scss";
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
   const { isCartOpen } = useContext(CartContext);
-
+  
     return (
       <Fragment>
       <div className='navigation'>
