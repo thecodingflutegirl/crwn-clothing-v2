7cd6f0b65324082aa3cf3f1c5f07ff1250209d06
Julia Evers <thatflutegirl12@gmail.com>
2023-04-18 16:06:23 -0400

    not sure, restarting react project again, updated to this point

---
+++ crwn-clothing/src/contexts/categories.context.jsx
@@ -0,0 +1,36 @@
+import { createContext, useState, useEffect } from 'react';
+
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils.js';
+
+// import SHOP_DATA from '../shop-data.js';
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      // console.log(categoryMap);
+      setCategoriesMap(categoryMap)
+    }
+    getCategoriesMap();
+   
+  }, []);
+
+// ONLY RAN THIS AS A ONE TIME THING. DO NOT NEED TO RUN IT EVERY TIME TO POPULATE FIRESTORE DATABASE
+  // useEffect(() => {
+  //   addCollectionAndDocuments('categories', SHOP_DATA);
+  // })
+
+
+  const value = { categoriesMap };
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
--- crwn-clothing/src/utils/firebase/firebase.utils.js
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -6,8 +6,9 @@ import { getAuth,
     createUserWithEmailAndPassword, 
     signInWithEmailAndPassword,
     signOut,
-    onAuthStateChanged } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc, Firestore } from 'firebase/firestore';
+    onAuthStateChanged,
+     } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs, Firestore } from 'firebase/firestore';
 import { redirect } from 'react-router-dom';
 
 
@@ -33,6 +34,33 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
 export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach((object) => {
+        const docRef = doc(collectionRef, object.title.toLowerCase());
+        batch.set(docRef, object);
+    });
+
+    await batch.commit();
+    console.log("done")
+};
+
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+        const { title, items } = docSnapshot.data();
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {});
+
+    return categoryMap; 
+}
+
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
     if(!userAuth) return;
